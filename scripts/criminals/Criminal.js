897c7768caca98e8427c7b15bc6ca4e1d08edab0
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-22 22:58:04 -0600

    Added styling to criminal data

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,11 +1,11 @@
 export const criminalCard = (criminalObj) => {
        return `
         <section>
-            <p>Name: ${criminalObj.name}</p>
+            <p>${criminalObj.name}</p>
             <p>Age: ${criminalObj.age}</p>
             <p>Crime: ${criminalObj.conviction}</p>
-            <p>Term Start: ${criminalObj.incarceration.start}</p>
-            <p>Term End: ${criminalObj.incarceration.end}</p>
+            <p>Term Start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</p>
+            <p>Term End: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</p>
         </section>
         `
 }
