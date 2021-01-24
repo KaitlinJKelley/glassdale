02df73c167ca555fae01784b35fcdb456a39f905
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-24 15:35:20 -0600

    Added remaining modules and functions to render officer data to DOM; adjusted CSS to only affect criminal html

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 console.log("Welcome to the main module")
 import { criminalList } from "./criminals/CriminalList.js"
-
+import { officerList } from "./officers/OfficerList.js"
 
 criminalList()
+officerList()
 
 
