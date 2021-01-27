0381ec8fd71909895736fe16e7ec445373c45d97
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-27 16:41:09 -0600

    Working on showing notes; still broken

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,13 @@ import { officerList } from "./officers/OfficerList.js"
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { officerSelect } from "./officers/OfficerSelect.js"
 import { NoteForm } from "./notes/NoteForm.js"
+import { ShowNoteButton } from "./notes/ShowNotesButton.js"
+import { NoteList } from "./notes/NoteList.js"
 
 ConvictionSelect()
 NoteForm()
 criminalList()
 officerList()
 officerSelect()
-
+ShowNoteButton()
 
