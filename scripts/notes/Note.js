f55a55c373db125b23c18bb94f05ca888439d599
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-28 15:07:33 -0600

    Finally got sync to work...

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,8 +1,8 @@
 export const NoteHTMLConverter = (noteObject) => {
     return `<section class="note">
-            <div class="note__text">${noteObject.noteText}</div>
-            <div class="note__date">Timestamp: ${new Date(noteObject.dateOfNote).toLocaleDateString('en-US')}</div>
-            <div class="note__suspect">Title: ${noteObject.suspectName}</div>
+            <div class="note__text">Note: ${noteObject.noteText}</div>
+            <div class="note__date">Date: ${new Date(noteObject.dateOfNote).toLocaleDateString('en-US')}</div>
+            <div class="note__suspect">Suspect: ${noteObject.suspectName}</div>
             </section>
             `
             // <div class="note__author">Author: ${ noteObject.author }</div>
