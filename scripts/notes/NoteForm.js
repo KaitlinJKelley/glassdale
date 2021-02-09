4426cf7b13ea2d365ec0f713c676c94de88ee68a
kjk1325e <kaitlinjkelley@gmail.comm>
2021-02-09 15:19:21 -0600

    Working on getting criminal id to work

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,23 +1,41 @@
+import { getCriminals, useCriminals } from "../criminals/CriminalProvider.js"
 import { NoteList } from "./NoteList.js"
 import { saveNote } from "./NoteProvider.js"
+
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
 
 
-export const render = () => {
-    // Added label tags for each input; do input tags have to have a label tag?
-    contentTarget.innerHTML = `
-            <textarea id="note-text" placeholder="Note"></textarea>
-            <input type="date" id="note-date"></input>
-            <input type="text" id="note-suspect" placeholder="Suspect Name"></input>
 
-            <button type="button" id="saveNote">Save Note</button>
-    `
+export const render = () => {
+    let optionTags = ""
+    // Creates option tags for each criminal
+        getCriminals()
+            .then(() => {
+                let criminals = useCriminals()
+    
+                optionTags += criminals.map((criminal) => {return `<option value="${ criminal.id }">${ criminal.name }</option>`})
+                
+                
+                // Puts NoteForm on the DOM
+                contentTarget.innerHTML = `
+                <textarea id="note-text" placeholder="Note"></textarea>
+                <input type="date" id="note-date"></input>
+                <select id="noteForm--criminal" class="criminalSelect">
+                ${optionTags}
+                </select>
+                
+                <button type="button" id="saveNote">Save Note</button>
+                `
+            })
 }
-export const NoteForm = () => {
+
+export const NoteForm = () => {  
+    // debugger
     render()
 }
+
 
 
 // Handle browser-generated click event in component
@@ -29,12 +47,33 @@ eventHub.addEventListener("click", clickEvent => {
             // Key/value pairs here
             noteText: document.querySelector("#note-text").value,
             dateOfNote: document.querySelector("#note-date").value,
-            suspectName: document.querySelector("#note-suspect").value
+            criminalId: selectedCriminalId
         }
 
         // Change API state and application state
         saveNote(newNote)
     }
+})
+
+
+eventHub.addEventListener("change", event => {
+    if (event.target.id === "noteForm--criminal") { 
+        let criminalId = event.target.value
+        
+        const customEvent = new CustomEvent("criminalSelected", {
+            detail: {
+                criminalId: criminalId
+            }
+        })
+    }
+
+        // Dispatch event to event hub
+        eventHub.dispatchEvent(customEvent)
+      
+})
+
+eventHub.addEventListener("criminalSelected", event => {
+     selectedCriminalId = event.detail.criminalId
 })
 
 
